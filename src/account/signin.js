b68ce647d9687e9da8c0ec80dd4e2b2c374ad904
Byungha Ko <rhqo123@gmail.com>
2023-05-14 15:11:07 +0900

    siginin 화면 수정

--- src/account/signin.js
+++ src/account/signin.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function SigninForm() {
   const [Id, setId] = useState('');
   const [Password, setPassword] = useState('');
+  const [checkbox, setCheckbox] = useState(false);
 
   const handleID = (event) => {
     setId(event.target.value);
@@ -11,10 +12,17 @@ function SigninForm() {
   const handlePW = (event) => {
     setPassword(event.target.value);
   }
+  const handleCheckbox = (event) => {
+    setCheckbox(event.target.value);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(JSON.stringify({ Id, Password }))
+    //alert(JSON.stringify({ Id, Password }))
+    if (Id === 'admin' && Password === '1234') {
+      alert('로그인 성공');
+      window.location.href = '/memo';
+    }
   }
 
     return (
@@ -29,6 +37,11 @@ function SigninForm() {
             <label for="floatingPassword">Password</label>
           </div>
         </label>
+        {/* <div style="height: 3px;"></div> */}
+            <div class="checkbox">
+                <input type="checkbox" value={checkbox} onChange={handleCheckbox} style={{margin: '3px'}}></input>
+                <label>Remember me</label>
+        </div>
         <input type="submit" class="w-100 btn btn-lg btn-primary mb-lg-5" value="Submit" />
       </form>
     );

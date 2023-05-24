2b4320dfd99dbc1215fa4c17e06feb3d24b7e396
Byungha Ko <rhqo123@gmail.com>
2023-05-24 14:15:12 +0900

    js directory 추가 (redux)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
+import store from './js/store/index';
+import { addArticle } from './js/actions';
+window.store = store;
+window.addArticle = addArticle;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
